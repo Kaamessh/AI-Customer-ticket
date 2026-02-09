0432129569236db76941bc5ca2bef614dc3395ec
balaji j <j.balaji2468@gmail.com>
2026-02-09 16:11:05 +0530

    add cors support
    Signed-off-by: balaji j <j.balaji2468@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.api import initialize_endpoints
 from src.database.core import Base, engine
 
@@ -10,6 +11,14 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(title="AI Customer Ticket", lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 initialize_endpoints(app)
 
